823c8252e53453aacc81697ae304924c3012dac9
Faro-Enuff <f.roth2503@gmail.com>
2021-09-07 21:46:26 +0200

    Key Adjustments

---
+++ src/utils/firbaseData.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  authDomain: "coco-not-feed.firebaseapp.com",
+  projectId: "coco-not-feed",
+  storageBucket: "coco-not-feed.appspot.com",
+  messagingSenderId: "536968916963",
+  appId: "1:536968916963:web:a078ddd2bcced1926d3ad6",
+};
